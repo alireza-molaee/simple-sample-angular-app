019119c7090f8ce2d80fed2238da89d02f122071
Alireza Molaee <alirezamolaii@gmail.com>
2016-07-12 20:09:50 +0430

    modify blog page

--- app/blog/blog.js
+++ app/blog/blog.js
@@ -10,5 +10,23 @@ angular.module('ssap.blog', ['ngRoute'])
     }])
 
     .controller('blogCtrl', function($scope, $route) {
+        var PageCount = Math.ceil(Posts.length / 10);
+        $scope.pageList = [];
+        for (var i = 1; i <= PageCount; i++){
+            $scope.pageList.push(i);
+        }
 
+        $scope.currentPage = 1;
+
+        $scope.goToPage = function (page) {
+            $scope.currentPage = page;
+        }
+
+        $scope.postsInCurrentPage = function () {
+            var pointToSlice = ($scope.currentPage - 1) * 10;
+            return Posts.slice(pointToSlice, pointToSlice+10)
+        }
+        
+        
+        
     });
